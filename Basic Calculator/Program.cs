33c7d18365f2c94d31010c717015697eb7898b79
Christine Arroyo <chrstinearroyo@gmail.com>
2024-09-29 20:41:54 +0800

    Update Program.cs

--- Basic Calculator/Program.cs
+++ Basic Calculator/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿Console.WriteLine("BASIC CALCULATOR by Christine Arroyo");
+Console.Write("Choose an Operations: 1 - Addition, 2 - Subtraction, 3 - Multiplication, 4 - Division ");
+Console.WriteLine(" ");
+Console.Write("Enter your chosen Operation: ");
+string operation = Console.ReadLine();
+
+Console.WriteLine("Enter the First Number:");
+int Num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the First Number:");
+int Num2 = int.Parse(Console.ReadLine());
+
+if (operation == "1")
+    {
+        int result = Num1 + Num2;
+        Console.WriteLine("Result: " + result + " is the answer");
+        Console.ReadLine();
+    }
+else if (operation == "2")
+    {
+        int result = Num1 - Num2;
+        Console.WriteLine("Result: " + result + " is the answer");
+        Console.ReadLine();
+}
+else if (operation == "3")
+    {
+        int result = Num1 * Num2;
+        Console.WriteLine("Result: " + result + " is the answer");
+        Console.ReadLine();
+}
+else if (operation == "4")
+    {
+        int result = Num1 / Num2;
+        Console.WriteLine("Result: " + result + " is the answer");
+        Console.ReadLine();
+    }
+
+
+
+
+
